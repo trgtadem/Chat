50c87e0f8506f4ed677056c9937e2b4e5ea7316b
Adem TURGUT <ademturgut1638@ornek.com>
2026-01-15 14:10:50 +0300

    düzeltme

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -5,13 +5,7 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import Constants from 'expo-constants'; // expo-constants'ı import et
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB7rAC1pAWYWPsuXW_BtCV-2RlLcbUuas4",
-  authDomain: "mobilapp-b46ba.firebaseapp.com",
-  projectId: "mobilapp-b46ba",
-  storageBucket: "mobilapp-b46ba.firebasestorage.app",
-  messagingSenderId: "610020027674",
-  appId: "1:610020027674:web:083f1a51b258579b6250a3",
-  measurementId: "G-C38FFN206H"
+
 };
 
 const app = initializeApp(firebaseConfig);
